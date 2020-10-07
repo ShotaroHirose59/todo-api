4ef700158d2f31e69e0949edcfd08e2ad1b66313
Shotaro Hirose <shotaro199959@icloud.com>
2020-10-07 16:14:41 +0900

    [Fix] CROS再設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' ## <= 修正。* でフルオープンにする。
+    ## origins '*' 
+    if Rails.env.production?
+        origins 'https://web-todo.firebase.web.app' 
+    else 
+        origins 'http://localhost:3000'
+    end
 
     resource '*',
       headers: :any,
